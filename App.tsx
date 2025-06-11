de41da319e79eea3e017cf5b85d2e34e23b26e1f
danielcueto <daniel.cueto@assuresoft.tech>
2025-06-11 09:05:37 -0400

    creating home component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import Home from './src/screens/Home';
 
 function App(): React.JSX.Element {
   return (
-    <View>
-      <Text> Hola React Native 2025</Text>
+    <View style={styles.mainContainer}>
+      <Home />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+});
 
 export default App;
