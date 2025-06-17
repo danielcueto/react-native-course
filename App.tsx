f7bb1db9ca322e7cca7af81c8c1fc1296b63ae5b
danielcueto <daniel.cueto@assuresoft.tech>
2025-06-17 08:55:22 -0400

    added react native navigation

--- App.tsx
+++ App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import {View} from 'react-native';
 import Home from './src/screens/Home';
 import ThemeProvider from './src/context/ThemeContext';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App(): React.JSX.Element {
-
-
   return (
     <ThemeProvider>
-    <View style={{flex: 1}}>
-      <Home />
-    </View>
+      <NavigationContainer>
+        <View style={{flex: 1}}>
+          <Home />
+        </View>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
